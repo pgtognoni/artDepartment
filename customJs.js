cf99b5ce4c15d83d651e19e703a323ef919304fe
pgtognoni <pgtognoni@gmail.com>
2024-05-21 22:13:51 +0100

    adding landing path

--- customJs.js
+++ customJs.js
@@ -1,38 +1,40 @@
 // LOADER FUNCTION FOR CUSTOM MSDOS LOADER 
+if (window.location.pathname === '/') {
+    let count = 0;
+    const percent = document.getElementById("percent");
+    const bar = document.getElementById("bar");
+    const loading = document.getElementById("loading");
+    const complete = document.getElementById("complete");
+    const container = document.getElementById("loader-container")
+    const loader = document.getElementById("loader")
 
-let count = 0;
-const percent = document.getElementById("percent");
-const bar = document.getElementById("bar");
-const loading = document.getElementById("loading");
-const complete = document.getElementById("complete");
-const container = document.getElementById("loader-container")
-const loader = document.getElementById("loader")
+    complete.style.opacity = 0;
 
-complete.style.opacity = 0;
+    function startLoader() {
+    percent.textContent = "/ " + count + "%";
+    nextLoader();
+    }
 
-function startLoader() {
-  percent.textContent = "/ " + count + "%";
-  nextLoader();
+    function nextLoader() {
+        bar.style.width = count + "%";
+    if (count < 100) {
+        count++;
+        setTimeout(startLoader, 20);
+    } else {
+        loading.style.opacity = 0;
+        complete.style.opacity = 1;
+        container.style.transition = "opacity 800ms 500ms";
+        container.style.opacity = 0;
+        loader.style.transition = "opacity 1200ms 1500ms";
+        loader.style.opacity = 0;
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 2000)
+    }
+    }
+
+    startLoader()
 }
-function nextLoader() {
- 	bar.style.width = count + "%";
-  if (count < 100) {
-    count++;
-    setTimeout(startLoader, 20);
-  } else {
-  	loading.style.opacity = 0;
-    complete.style.opacity = 1;
-    container.style.transition = "opacity 800ms 500ms";
-    container.style.opacity = 0;
-    loader.style.transition = "opacity 1200ms 1500ms";
-    loader.style.opacity = 0;
-   	setTimeout(() => {
-    	loader.style.display = "none";
-    }, 2000)
-  }
-}
-
-startLoader()
 
 // CUSTOM FUNCTION TO PLAY SOUND - ON / OFF
 
@@ -53,12 +55,12 @@ if (off) {
 const iframe1 =  document.getElementById('vimeo-player-1');
 let vimeoPlayer = null;
 if (iframe1) {
-  vimeoPlayer = new Vimeo.Player(iframe)
+  vimeoPlayer = new Vimeo.Player(iframe1)
   if (vimeoPlayer) {
      vimeoPlayer.setVolume(0);
   }
-  }
-  let isMuted = true
+}
+let isMuted = true
 
 // Function to toggle mute/unmute state
 function toggleMute() {
