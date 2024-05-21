cad32b0b111b4a42d6981431b115c0c260f4efab
pgtognoni <pgtognoni@gmail.com>
2024-05-21 22:07:38 +0100

    adding sound video-1

--- customJs.js
+++ customJs.js
@@ -1,3 +1,5 @@
+// LOADER FUNCTION FOR CUSTOM MSDOS LOADER 
+
 let count = 0;
 const percent = document.getElementById("percent");
 const bar = document.getElementById("bar");
@@ -31,3 +33,49 @@ function nextLoader() {
 }
 
 startLoader()
+
+// CUSTOM FUNCTION TO PLAY SOUND - ON / OFF
+
+// Get the Vimeo iframe element
+const sound = document.getElementById('sound-on-off');
+const on = document.querySelector('.sound-on');
+let onClasses;
+if (on) {
+  onClasses = on.classList;
+  on.classList.add('hidden');
+}
+const off = document.querySelector('.sound-off');
+let offClasses;
+if (off) {
+  offClasses = off.classList;
+}
+
+const iframe1 =  document.getElementById('vimeo-player-1');
+let vimeoPlayer = null;
+if (iframe1) {
+  vimeoPlayer = new Vimeo.Player(iframe)
+  if (vimeoPlayer) {
+     vimeoPlayer.setVolume(0);
+  }
+  }
+  let isMuted = true
+
+// Function to toggle mute/unmute state
+function toggleMute() {
+      onClasses.toggle('hidden');
+  offClasses.toggle('hidden');
+  // Toggle mute state
+  if (isMuted === true) {
+    // If muted, unmute the video
+    vimeoPlayer.setVolume(0.5);
+    isMuted = false
+  } else {
+    // If not muted, mute the video
+    vimeoPlayer.setVolume(0);
+    isMuted = true
+  }
+}
+// Add click event listener to the Vimeo iframe
+if (sound) {
+    sound.addEventListener('click', toggleMute);
+}
